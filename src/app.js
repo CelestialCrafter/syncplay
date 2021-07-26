4b7f981dd317de09c7716886873caff9b8b1903a
CelestialCrafter <celestialcraftermc@gmail.com>
2021-07-26 17:55:21 -0400

    upload files

---
+++ src/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const io = require('socket.io')(httpServer);
+
+const PlayerState = { UNSTARTED: -1, ENDED: 0, PLAYING: 1, PAUSED: 2, BUFFERING: 3, CUED: 5 };
+const roomData = {};
+
+io.on('connection', socket => {
+	socket.room = socket.id;
+	if (!roomData[socket.room]) roomData[socket.room] = { video: null };
+	socket.emit('currentRoom', socket.room);
+
+	socket.on('joinRoom', room => {
+		if (io.sockets.adapter.rooms.has(room)) socket.join(room);
+		else return socket.emit('error', { error: 'Room does not exist' });
+		socket.room = room;
+
+		if (!roomData[socket.room]) roomData[socket.room] = { video: null };
+
+		const video = roomData[socket.room].video;
+		if (video) socket.emit('roomVideo', video);
+		socket.emit('currentRoom', socket.room);
+	});
+
+	socket.on('updateVideo', video => {
+		if (!socket.room) return socket.emit('error', { error: 'No Room' });
+
+		const videoId = video.replace(/.+\?v=/, '');
+		if (!video) return socket.emit('error', { error: 'No Video' });
+		roomData[socket.room].video = videoId;
+
+		io.to(socket.room).emit('roomVideo', roomData[socket.room].video);
+	});
+
+	socket.on('stateChange', data => {
+		if (!socket.room) return socket.emit('error', { error: 'No Room' });
+		let updatedData = data;
+
+		// Converts all states to play/pause
+		switch (data.state) {
+			case PlayerState.UNSTARTED:
+				updatedData = { ...updatedData, time: 0, state: PlayerState.PAUSED };
+				break;
+			case PlayerState.BUFFERING || PlayerState.ENDED:
+				updatedData = { ...updatedData, state: PlayerState.PAUSED };
+		}
+
+		socket.to(socket.room).emit('updateState', updatedData);
+	});
+});
+
+app.use(express.static(path.resolve('public/')));
+
+httpServer.listen(8080);
---
+++ src/public/app.js
@@ -0,0 +1,95 @@
+/* eslint-disable no-undef */
+
+let player;
+let recentlyUpdated = false;
+let prevState = -1;
+let prevTime = 0;
+const socket = io(window.location.origin);
+
+const videoInput = document.getElementById('videoInput');
+const roomInput = document.getElementById('roomInput');
+const submitVideo = document.getElementById('submitVideo');
+const submitRoom = document.getElementById('submitRoom');
+const roomId = document.getElementById('roomId');
+const currentRoomId = document.getElementById('currentRoomId');
+
+function onYouTubeIframeAPIReady() {
+	submitRoom.addEventListener('click', event => {
+		event.preventDefault();
+		socket.emit('joinRoom', roomInput.value);
+	});
+
+	submitVideo.addEventListener('click', event => {
+		event.preventDefault();
+		socket.emit('updateVideo', videoInput.value);
+	});
+
+	socket.on('roomVideo', videoId => {
+		player?.destroy();
+
+		player = new YT.Player('player', {
+			width: window.innerWidth / 2 - 30,
+			height: window.innerHeight / 2,
+			videoId,
+			playerVars: {
+				'playsinline': 1
+			},
+			events: {
+				'onReady': onPlayerReady,
+				'onStateChange': onPlayerStateChange
+			}
+		});
+
+		M.toast({ html: '<span class="green-text">Changed Video!</span>' });
+	});
+}
+
+const onPlayerReady = event => {
+	player.playVideo();
+
+	setInterval(() => {
+		socket.emit('stateChange', {
+			state: player.getPlayerState(),
+			time: player.getCurrentTime()
+		});
+	}, 2000);
+
+	socket.on('updateState', data => {
+		const state = player.getPlayerState();
+		const time = player.getCurrentTime();
+
+		// Make the video smoother
+		if (time !== data.time) player.seekTo(data.time, true);
+		if (data.state === YT.PlayerState.PLAYING) player.playVideo();
+		else player.pauseVideo();
+
+		recentlyUpdated = true;
+		setTimeout(() => recentlyUpdated = false, 100);
+	});
+};
+
+const onPlayerStateChange = event => {
+	const time = player.getCurrentTime();
+
+	// Prevent the state from spam updating
+	if (recentlyUpdated) return;
+	if (event.data === prevState) return;
+	// Only update if skipped ahead atleast 1 second
+	if (!(time - prevTime > 1 && time - prevTime < 1)) return;
+
+	socket.emit('stateChange', {
+		state: event.data,
+		time
+	});
+};
+
+socket.on('error', data => M.toast({ html: `<span class="red-text">Error: ${data.error}</span>` }));
+socket.on('connect', () => {
+	M.toast({ html: '<span class="green-text">Connected to server!</span>' });
+	roomId.innerText = socket.id;
+});
+socket.on('disconnect', () => M.toast({ html: '<span class="yellow-text">Disconnected from server</span>' }));
+socket.on('currentRoom', room => {
+	socket.id === room ? currentRoomId.innerText = 'Your Room' : currentRoomId.innerText = room;
+	M.toast({ html: '<span class="green-text">Joined Room!</span>' });
+});
